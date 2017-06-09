9510a30e52d473f79c9818c10243c226dc2d083b
Reza Sherafat <sherafat@gmail.com>
2017-06-08 22:46:14 -0700

    Refactored bash script to create droplets with ability to query available SSH keys.

--- digitalocean/create_droplet.sh
+++ digitalocean/create_droplet.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-export auth_token="FILL IN HERE WITH YOUR W/R AUTH TOKEN";
+set -o errexit
+# set -o nounset
+set -o pipefail
 
-export droplet_ssh_key="FILL IN HERE WITH YOUR SSH KEY";
-export droplet_ssh_key_name="Reza id_rsa";
+export auth_token="$1";
+export ssh_key_name="$2";
 
 export droplet_img="docker";
 
@@ -13,20 +15,77 @@ export droplet_name_prefix="octave-docker";
 export droplet_tag="octave-docker-fleet";
 export droplet_name="$droplet_name_prefix-00";
 
-droplet_ssh_key_id=$(curl -s -X GET "https://api.digitalocean.com/v2/account/keys" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $auth_token" \
-    | jq -r ' .ssh_keys[].fingerprint ')
+# Return the fingerprint of designated SSH key.
+# SSH key must already be registered to create droplets
+function getSshKeyFingerpringWithName()
+{
+    local auth_token=$1;
+    local ssh_key_name=$2;
 
-echo "Using ssh key fingerprint: '$droplet_ssh_key_id'";
+    ssh_key_fingerprint=$(curl -s -X GET "https://api.digitalocean.com/v2/account/keys" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $auth_token" \
+        | jq -r " .ssh_keys[] | select( .name == \"$ssh_key_name\" ) | .fingerprint ");
 
-# curl -s -X POST "https://api.digitalocean.com/v2/account/keys" \
-#     -H "Content-Type: application/json" \
-#     -H "Authorization: Bearer $auth_token" \
-#     -d "{\"id\":\"$droplet_ssh_key_name\",\"public_key\":\"$droplet_ssh_key\"}"
+    echo $ssh_key_fingerprint;
+}
 
-curl -s -X POST "https://api.digitalocean.com/v2/droplets" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $auth_token" \
-    -d "{\"name\":\"$droplet_name\",\"region\":\"$droplet_region\",\"size\":\"$droplet_size\",\"image\":\"$droplet_img\",\"ssh_keys\":[\"$droplet_ssh_key_id\"],\"backups\":false,\"ipv6\":false,\"user_data\":null,\"private_networking\":null,\"volumes\":null,\"tags\":[\"$droplet_tag\"]}"
+# Returns all registered SSH keys
+function getRegisteredSshKeyNames()
+{
+    local auth_token=$1;
+    
+    response=$(curl -s -X GET "https://api.digitalocean.com/v2/account/keys" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $auth_token");
 
+    echo $response | jq -r " .ssh_keys[].name ";
+}
+
+function createDroplet() 
+{
+    local auth_token="$1";
+    local ssh_key_fingerprint="$2";
+    local droplet_name="$3";
+    local droplet_region="$4";
+    local droplet_size="$5";
+    local droplet_img="$6";
+    local droplet_tag="$7";
+
+    response=$(curl -s -X POST "https://api.digitalocean.com/v2/droplets" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $auth_token" \
+        -d "{\"name\":\"$droplet_name\",\"region\":\"$droplet_region\",\"size\":\"$droplet_size\",\"image\":\"$droplet_img\",\"ssh_keys\":[\"$ssh_key_fingerprint\"],\"backups\":false,\"ipv6\":false,\"user_data\":null,\"private_networking\":null,\"volumes\":null,\"tags\":[\"$droplet_tag\"]}");
+
+    echo $response | jq  -r ' .droplet.tags '
+}
+
+function printUsage()
+{
+    echo "Usage: $(basename $0) <AUTH_TOKEN> <SSH_KEY_NAME>"
+}
+
+# Check essential variables
+if [ "$auth_token" == "" ]; then
+    printUsage;
+    exit 1;
+fi
+
+if [ "$ssh_key_name" == "" ]; then
+    printUsage;
+    echo "Available keys are: $(getRegisteredSshKeyNames \"$auth_token\")";
+    exit 1;
+fi
+
+# Grab fingerprint of current SSH key registered with Digital Ocean
+ssh_key_fingerprint=$(getSshKeyFingerpringWithName "$auth_token" "$ssh_key_name");
+echo "Using ssh key fingerprint: '$ssh_key_fingerprint'";
+
+# Create droplet with spec
+createDroplet "$auth_token" \
+    "$ssh_key_fingerprint" \
+    "$droplet_name" \
+    "$droplet_region" \
+    "$droplet_size" \
+    "$droplet_img" \
+    "$droplet_tag"
